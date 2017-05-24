d85a283f3857e4d3ff037f58ff15a3a53a83f9dd
Jake Teton-Landis <jake.tl@airbnb.com>
2017-05-23 22:59:41 -0700

    library of common commands starts with a command factory

---
+++ commands/cmd.go
@@ -0,0 +1,59 @@
+package command
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+// LongFlagPrefix is the default prefix appended to multi-character flags in a command.
+var LongFlagPrefix = "--"
+
+// ShortFlagPrefix The default prefix applied to single-character flags in a command.
+var ShortFlagPrefix = "-"
+
+// Cmd is a factory (with decent erganomics) for creating *exec.Cmd instances.
+type Cmd struct {
+	Path            string
+	Flags           map[string]string
+	Args            []string
+	LongFlagPrefix  *string
+	ShortFlagPrefix *string
+}
+
+func (cmd *Cmd) applyDefaults() {
+	if cmd.LongFlagPrefix == nil {
+		cmd.LongFlagPrefix = &LongFlagPrefix
+	}
+	if cmd.ShortFlagPrefix == nil {
+		cmd.ShortFlagPrefix = &ShortFlagPrefix
+	}
+}
+
+// Build an *exec.Cmd from this Cmd. This returns a new *exec.Cmd on each call,
+// allowing you to use a Cmd as a factory.
+func (cmd *Cmd) Build() *exec.Cmd {
+	cmd.applyDefaults()
+
+	out := new(exec.Cmd)
+	out.Path = cmd.Path
+	out.Args = make([]string, len(cmd.Args)+len(cmd.Flags)*2)
+
+	// --foo bar
+	// -f bar
+	for key, value := range cmd.Flags {
+		prefix := cmd.LongFlagPrefix
+		if len(key) == 1 {
+			prefix = cmd.ShortFlagPrefix
+		}
+		out.Args = append(out.Args, *prefix+key, value)
+	}
+
+	out.Args = append(out.Args, cmd.Args...)
+
+	return out
+}
+
+func (cmd *Cmd) String() string {
+	return fmt.Sprintf("%T{%s %v %v}",
+		cmd, cmd.Path, cmd.Flags, cmd.Args)
+}
